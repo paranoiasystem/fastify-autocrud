a34c2288f5aef809cc28a997282410fd072f6875
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2019-08-19 18:57:11 +0200

    firt version work

--- example/server.js
+++ example/server.js
@@ -1,5 +1,3 @@
-'use strict'
-
 const fastify = require('fastify')({
     logger: true
 })
--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-'use strict'
-
 const fp = require('fastify-plugin')
 const boom = require('boom')
 
-async function fastifyAutoCRUD (fastify, opts, next) {
-    fastify.get('/', opts, async (req, reply) => {
+function plugin(fastify, opts, next) {
+
+    fastify.get(opts.prefix, opts, async (req, reply) => {
         try {
             reply.type('application/json').code(200).send(await opts.Collection.find())
         } catch(err) {
@@ -12,7 +11,7 @@ async function fastifyAutoCRUD (fastify, opts, next) {
         }
     })
 
-    fastify.get('/:id', opts, async (req, reply) => {
+    fastify.get(opts.prefix + '/:id', opts, async (req, reply) => {
         try {
             reply.type('application/json').code(200).send(await opts.Collection.findById(req.params.id))
         } catch(err) {
@@ -20,7 +19,7 @@ async function fastifyAutoCRUD (fastify, opts, next) {
         }
     })
 
-    fastify.post('/', opts, async (req, reply) => {
+    fastify.post(opts.prefix, opts, async (req, reply) => {
         try { 
             reply.type('application/json').code(201).send(await new opts.Collection(req.body).save())
         } catch(err) {
@@ -28,7 +27,7 @@ async function fastifyAutoCRUD (fastify, opts, next) {
         }
     })
 
-    fastify.put('/:id', opts, async (req, reply) => {
+    fastify.put(opts.prefix + '/:id', opts, async (req, reply) => {
         try {
             reply.type('application/json').code(200).send(await opts.Collection.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true }))
         } catch(err) {
@@ -36,7 +35,7 @@ async function fastifyAutoCRUD (fastify, opts, next) {
         }
     })
 
-    fastify.delete('/:id', opts, async(req, reply) => {
+    fastify.delete(opts.prefix + '/:id', opts, async(req, reply) => {
         try {
             reply.type('application/json').code(204).send(await opts.Collection.deleteOne({ _id: req.params.id }))
         } catch(err) {
@@ -46,15 +45,14 @@ async function fastifyAutoCRUD (fastify, opts, next) {
 
     if(opts.additionalRoute) {
         opts.additionalRoute.forEach(additional => {
+            additional.url = opts.prefix + additional.url
             fastify.route(additional)
         })
     }
-
+    
     next()
 }
 
-/*module.exports = fp(fastifyAutoCRUD, {
+module.exports = fp(plugin, {
     name: 'fastify-autocrud'
-})*/
-
-module.exports = fastifyAutoCRUD
+})
