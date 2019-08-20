fe516b8b550d387391e80fc690489159c4618d6a
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2019-08-21 00:31:15 +0200

    clean example

--- example/controllers/productController.js
+++ example/controllers/productController.js
@@ -12,36 +12,4 @@ const customRoute = {
   }
 }
 
-/* const test = {
-  method: 'GET',
-  url: '/test',
-  handler: async function (req, reply) {
-    var schema = require('../models/Product').schema.paths
-    var test = {
-      response: {
-        '200': {
-          type: 'object',
-          properties: {}
-        }
-      }
-    }
-    for (var field in schema) {
-      console.log(schema[field].options.type)
-      test.response[200].properties[field] = {}
-      switch (schema[field].instance) {
-        case 'ObjectID':
-          test.response[200].properties[field]['type'] = 'string'
-          break
-        case 'Date':
-          test.response[200].properties[field]['type'] = 'string'
-          break
-        default:
-          test.response[200].properties[field]['type'] = schema[field].instance.toLowerCase()
-          break
-      }
-    }
-    reply.type('application/json').code(200).send(test)
-  }
-} */
-
 module.exports = [customRoute]
